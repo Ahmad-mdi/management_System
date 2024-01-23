039573a2042447e5d4a4f92ae87b76e614bdbdad
AhmadMdi <admimohammadi@gmail.com>
2024-01-23 14:22:42 +0330

    edit appExceptions

--- src/main/java/com/manage/utils/exceptionHandlar/AppExceptionHandler.java
+++ src/main/java/com/manage/utils/exceptionHandlar/AppExceptionHandler.java
@@ -1,10 +1,9 @@
-package com.manage.exception_utils;
+package com.manage.utils.exceptionHandlar;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
