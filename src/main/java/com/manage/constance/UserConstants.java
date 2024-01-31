2ea34954b84602d382a4b9666c7baa8b112ff43f
AhmadMdi <admimohammadi@gmail.com>
2024-02-01 00:45:24 +0330

    set text error for invalid id users

--- src/main/java/com/manage/constance/UserConstants.java
+++ src/main/java/com/manage/constance/UserConstants.java
@@ -8,4 +8,5 @@ public class UserConstants {
     public static final String INVALID_NATIONAL_CODE = "National code already exists!";
     public static final String INVALID_USERNAME_OR_PASSWORD = "Incorrect username or password";
     public static final String DATA_INTEGRITY = "Data integrity violation occurred";
+    public static final String DATA_NOT_FOUND = "data with %id% not found";
 }
