6e8b2321983c250b7e0c50c045df732ae83233f4
AhmadMdi <admimohammadi@gmail.com>
2024-02-02 01:05:13 +0330

    edit error text and set usernotfound

--- src/main/java/com/manage/constance/UserConstants.java
+++ src/main/java/com/manage/constance/UserConstants.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserConstants {
     //errors:
-    public static final String INVALID_NATIONAL_CODE = "National code already exists!";
-    public static final String INVALID_USERNAME_OR_PASSWORD = "Incorrect username or password";
-    public static final String DATA_INTEGRITY = "Data integrity violation occurred";
-    public static final String DATA_NOT_FOUND = "data with %id% not found";
+    public static final String INVALID_NATIONAL_CODE = "national code already exists!";
+    public static final String INVALID_USERNAME_OR_PASSWORD = "incorrect username or password";
+    public static final String USER_NOT_FOUND = "data with username: %username% not found";
+    public static final String DATA_INTEGRITY = "data integrity violation occurred";
+    public static final String DATA_NOT_FOUND = "data with id %id% not found";
 }
