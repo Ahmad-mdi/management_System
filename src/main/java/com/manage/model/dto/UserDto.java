aae435c48350d1b4a4eeac4a7858e1c7d03adf3b
AhmadMdi <admimohammadi@gmail.com>
2024-02-09 23:05:31 +0330

    set regexp for changePassword

--- src/main/java/com/manage/model/dto/UserDto.java
+++ src/main/java/com/manage/model/dto/UserDto.java
@@ -5,15 +5,18 @@ import com.manage.annotation.NationalCode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.MessageSource;
 
 import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Locale;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-//    @JsonIgnore
     private long id;
     private String fullName;
     @NotBlank(message = "the username failed is required")
@@ -26,8 +29,9 @@ public class UserDto {
     @NotBlank(message = "the lastname failed is required")
     private String lastname;
     @NotBlank(message = "the password failed is required")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "password must be a combination of uppercase letters, lowercase letters, numbers, and special characters")
     private String password;
     private String newPassword;
     private String token;
-
 }
+
